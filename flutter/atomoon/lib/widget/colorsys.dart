45890d6e9ca9051c1a5bcc69dfb77c9d3344e168
Gabriel Santana <gabriel04.ok@gmail.com>
2022-08-23 03:10:02 -0300

    Novas cores incrementadas
    Co-authored-by: TAYLOR MATHEUS BURGUES GOMES <cc21452@m.unicamp.br>
    Co-authored-by: MARIA CLARA REDIN BENITEZ <maria.xuh@outlook.com>

--- flutter/atomoon/lib/widget/colorsys.dart
+++ flutter/atomoon/lib/widget/colorsys.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class Colorsys {
-  static Color  lightGrey = Color.fromRGBO(238, 238, 238, 1);
-  static Color  darkGrey = Color.fromRGBO(87, 87, 87, 1);
+  static Color cyan = Color.fromRGBO(46,205,240, 1);
+  static Color orange = Color.fromRGBO(243, 99, 67, 1);
+  static Color grey = Color.fromRGBO(112, 112, 112, 1);
+  static Color lightGrey = Color.fromRGBO(238, 238, 238, 1);
+  static Color darkGray = Color.fromRGBO(87, 87, 87, 1);
+  static Color black = Color.fromRGBO(57, 57, 57, 1);
 }
