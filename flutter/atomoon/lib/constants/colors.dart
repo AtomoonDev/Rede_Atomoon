f9f49a870c882886fc9bf6050fea077cfe3f369b
Gabriel Santana <gabriel04.ok@gmail.com>
2022-10-08 23:39:22 -0300

    🔧 | Constants implemented

---
+++ flutter/atomoon/lib/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kMainColor = Color.fromARGB(255, 36, 58, 105);
+const kBackgroundColor = Color(0xFFFAFAFA);
+
+class Colorsys {
+  static const Color cyan = Color.fromRGBO(46,205,240, 1);
+  static const Color orange = Color.fromRGBO(243, 99, 67, 1);
+  static const Color grey = Color.fromRGBO(112, 112, 112, 1);
+  static const Color lightGrey = Color.fromRGBO(238, 238, 238, 1);
+  static const Color darkGray = Color.fromRGBO(87, 87, 87, 1);
+  static const Color black = Color.fromRGBO(57, 57, 57, 1);
+}
