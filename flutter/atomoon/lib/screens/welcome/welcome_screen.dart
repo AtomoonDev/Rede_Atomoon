f25d08c0da5f374a3d8e819c62dca4bd9c6a20e3
Gabriel Santana <gabriel04.ok@gmail.com>
2022-10-09 00:34:59 -0300

    💼 | Adicionado telas de boas-vindas e seleção do tipo de usuário (Trabalhador ou Consumidor)
    Co-authored-by: TAYLOR MATHEUS BURGUES GOMES <cc21452@m.unicamp.br>
    Co-authored-by: MARIA CLARA REDIN BENITEZ <maria.xuh@outlook.com>

---
+++ flutter/atomoon/lib/screens/welcome/welcome_screen.dart
@@ -0,0 +1,84 @@
+import 'package:atomoon/screens/signIn_or_signUp/signin_or_signup_screen.dart';
+import 'package:flutter/material.dart';
+
+import 'package:atomoon/constants.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container( 
+          alignment: Alignment.center,/*
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Color.fromRGBO(8, 22, 49, 1), Color.fromRGBO(148, 158, 255, 1)] ))
+                ,*/
+          child: Column(
+          children: [
+            Spacer(flex: 2),
+            Image.asset("lib/assets/images/lua_semborda1.png"),
+            Spacer(flex: 3),
+            Text(
+              "Aplicativo Atomoon",
+              // 
+              textAlign: TextAlign.center,
+              style: Theme.of(context)
+                  .textTheme
+                  .headline5!
+                  .copyWith(fontWeight: FontWeight.bold),
+            ),
+            Spacer(),
+            Text(
+              "O App para buscar trabalhos em sua área.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Theme.of(context)
+                    .textTheme
+                    .bodyText1!
+                    .color!
+                    .withOpacity(0.64),
+              ),
+            ),
+            Spacer(flex: 3),
+            FittedBox(
+              child: TextButton(
+                  onPressed: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => SigninOrSignupScreen(),
+                        ),
+                      ),
+                  child: Row(
+                    children: [
+                      Text(
+                        "Pular",
+                        style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                              color: Theme.of(context)
+                                  .textTheme
+                                  .bodyText1!
+                                  .color!
+                                  .withOpacity(0.8),
+                            ),
+                      ),
+                      SizedBox(width: kDefaultPadding / 4),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        size: 16,
+                        color: Theme.of(context)
+                            .textTheme
+                            .bodyText1!
+                            .color!
+                            .withOpacity(0.8),
+                      )
+                    ],
+                  )),
+            )
+          ],
+        ),),
+      ),
+    );
+  }
+}
