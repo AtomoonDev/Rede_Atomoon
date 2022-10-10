ab69b151c8e71dfc14e262e96cee6f118eca49f5
PuniGC <gabriel04.ok@gmail.com>
2022-10-10 17:09:01 -0300

    👾 | Corrigido erro que não aparecia a tela de categorias
    Co-authored-by: TAYLOR MATHEUS BURGUES GOMES <theo_xone@outlook.com>
    Co-authored-by: MARIA CLARA REDIN BENITEZ <maria.xuh@outlook.com>

--- flutter/atomoon/lib/screens/signIn_or_signUp/login_screen.dart
+++ flutter/atomoon/lib/screens/signIn_or_signUp/login_screen.dart
@@ -1,145 +1,145 @@
-import 'package:atomoon/screens/home/home_screen.dart';
-import 'package:flutter/material.dart';
+  import 'package:atomoon/screens/screens.dart';
+  import 'package:flutter/material.dart';
 
-class Login extends StatefulWidget {
-  const Login({Key? key}) : super(key: key);
+  class Login extends StatefulWidget {
+    const Login({Key? key}) : super(key: key);
 
-  @override
-  State<Login> createState() => _LoginState();
-}
+    @override
+    State<Login> createState() => _LoginState();
+  }
 
-class _LoginState extends State<Login> {
-  String? _password = " ";
-  bool esconderTexto = true;
-  bool esconderTexto2 = true;
-  var formKey = GlobalKey<FormState>();
-  Color shadowColor = Colors.black;
+  class _LoginState extends State<Login> {
+    String? _password = " ";
+    bool esconderTexto = true;
+    bool esconderTexto2 = true;
+    var formKey = GlobalKey<FormState>();
+    Color shadowColor = Colors.black;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          actions: [
-            IconButton(
-              color: Colors.black,
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.pop(context);
-              },
-            ),
-            Expanded(child: Container()),
-          ],
-        ),
-        body: Center(
-            child: Form(
-                key: formKey,
-                child: Container(
-                    margin: EdgeInsets.all(20),
-                    child: Wrap(
-                      alignment: WrapAlignment.center,
-                      spacing: 20,
-                      runSpacing: 20,
-                      children: [
-                        Container(
-                            alignment: Alignment.topLeft,
-                            padding: EdgeInsets.only(bottom: 0),
-                            child: Column(children: [
-                              Text(
-                                "Entrar",
-                                style: const TextStyle(
-                                    fontWeight: FontWeight.bold, fontSize: 35),
-                              ),
-                            ])),
-                        Material(
-                          elevation: 8,
-                          shadowColor: shadowColor,
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                              labelText: 'E-mail',
-                              prefixIcon: Padding(
-                                child: Icon(Icons.email_rounded,
-                                    color: Color.fromRGBO(36, 58, 105, 1)),
-                                padding: EdgeInsets.all(5),
-                              ),
-                            ),
-                            validator: (String? value) {
-                              if (value == null || value.isEmpty) {
-                                setState(() {
-                                  shadowColor = Colors.transparent;
-                                });
-                                return 'E-mail Obrigatório';
-                              }
-                              setState(() {
-                                shadowColor = Colors.black;
-                              });
-                            },
-                          ),
-                        ),
-                        Material(
-                          elevation: 8,
-                          shadowColor: shadowColor,
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                              labelText: 'Senha',
-                              prefixIcon: Padding(
-                                child: Icon(
-                                  Icons.key,
-                                  color: Color.fromRGBO(36, 58, 105, 1),
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+            actions: [
+              IconButton(
+                color: Colors.black,
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              Expanded(child: Container()),
+            ],
+          ),
+          body: Center(
+              child: Form(
+                  key: formKey,
+                  child: Container(
+                      margin: EdgeInsets.all(20),
+                      child: Wrap(
+                        alignment: WrapAlignment.center,
+                        spacing: 20,
+                        runSpacing: 20,
+                        children: [
+                          Container(
+                              alignment: Alignment.topLeft,
+                              padding: EdgeInsets.only(bottom: 0),
+                              child: Column(children: [
+                                Text(
+                                  "Entrar",
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.bold, fontSize: 35),
                                 ),
-                                padding: EdgeInsets.all(5),
+                              ])),
+                          Material(
+                            elevation: 8,
+                            shadowColor: shadowColor,
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                labelText: 'E-mail',
+                                prefixIcon: Padding(
+                                  child: Icon(Icons.email_rounded,
+                                      color: Color.fromRGBO(36, 58, 105, 1)),
+                                  padding: EdgeInsets.all(5),
+                                ),
                               ),
-                              suffixIcon: IconButton(
-                                  icon: Icon(
-                                    Icons.remove_red_eye_outlined,
+                              validator: (String? value) {
+                                if (value == null || value.isEmpty) {
+                                  setState(() {
+                                    shadowColor = Colors.transparent;
+                                  });
+                                  return 'E-mail Obrigatório';
+                                }
+                                setState(() {
+                                  shadowColor = Colors.black;
+                                });
+                              },
+                            ),
+                          ),
+                          Material(
+                            elevation: 8,
+                            shadowColor: shadowColor,
+                            child: TextFormField(
+                              decoration: InputDecoration(
+                                labelText: 'Senha',
+                                prefixIcon: Padding(
+                                  child: Icon(
+                                    Icons.key,
                                     color: Color.fromRGBO(36, 58, 105, 1),
                                   ),
-                                  onPressed: (() => setState(() {
-                                        esconderTexto = !esconderTexto;
-                                      }))),
-                            ),
-                            validator: (String? value) {
-                              if (value == null || value.isEmpty) {
-                                setState(() {
-                                  shadowColor = Colors.transparent;
-                                });
-                                return 'Senha obrigatória';
-                              }
-                              setState(() {
-                                shadowColor = Colors.black;
-                              });
-                            },
-                            onSaved: (val) => _password = val,
-                            obscureText: esconderTexto,
-                          ),
-                        ),
-                        Container(
-                          margin:
-                              (EdgeInsets.only(top: 15, left: 25, right: 25)),
-                          height: 50,
-                          width: 200,
-                          decoration: BoxDecoration(
-                              color: Color.fromRGBO(36, 58, 105, 1),
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(32))),
-                          child: TextButton(
-                              child: Center(
-                                child: Text(
-                                  'Criar conta',
-                                  style: TextStyle(color: Colors.white),
+                                  padding: EdgeInsets.all(5),
                                 ),
+                                suffixIcon: IconButton(
+                                    icon: Icon(
+                                      Icons.remove_red_eye_outlined,
+                                      color: Color.fromRGBO(36, 58, 105, 1),
+                                    ),
+                                    onPressed: (() => setState(() {
+                                          esconderTexto = !esconderTexto;
+                                        }))),
                               ),
-                              onPressed: () {
-                                if (formKey.currentState?.validate() ?? false) {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => HomeScreen()));
+                              validator: (String? value) {
+                                if (value == null || value.isEmpty) {
+                                  setState(() {
+                                    shadowColor = Colors.transparent;
+                                  });
+                                  return 'Senha obrigatória';
                                 }
-                              }),
-                        ),
-                      ],
-                    )))));
+                                setState(() {
+                                  shadowColor = Colors.black;
+                                });
+                              },
+                              onSaved: (val) => _password = val,
+                              obscureText: esconderTexto,
+                            ),
+                          ),
+                          Container(
+                            margin:
+                                (EdgeInsets.only(top: 15, left: 25, right: 25)),
+                            height: 50,
+                            width: 200,
+                            decoration: BoxDecoration(
+                                color: Color.fromRGBO(36, 58, 105, 1),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(32))),
+                            child: TextButton(
+                                child: Center(
+                                  child: Text(
+                                    'Criar conta',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                                onPressed: () {
+                                  if (formKey.currentState?.validate() ?? false) {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => CategoriesScreen()));
+                                  }
+                                }),
+                          ),
+                        ],
+                      )))));
+    }
   }
-}
